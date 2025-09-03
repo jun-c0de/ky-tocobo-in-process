aedb5f5d55072f8a7e3462beabd373fa751df1e0
jun-c0de <sh48520414@gmail.com>
2025-09-03 21:47:29 +0900

    cc

--- src/sections/Collection.jsx
+++ src/sections/Collection.jsx
@@ -7,21 +7,43 @@ import 'swiper/css/pagination'
 import "../styles/sections/collection.scss"
 import collectionData from '../util/collection'
 const Collection = () => {
+    const prevRef = useRef(null)
+    const nextRef = useRef(null)
+    const swiperRef = useRef(null)
+
+    useEffect(() => {
+
+        if (swiperRef.current &&
+            swiperRef.current.params &&
+            prevRef.current &&
+            nextRef.current
+        ) {
+            swiperRef.current.params.navigation.prevEl = prevRef.current
+            swiperRef.current.params.navigation.nextEl = nextRef.current
+            swiperRef.current.navigation.destroy()
+            swiperRef.current.navigation.init()
+            swiperRef.current.navigation.update()
+        }
+
+    }, [])
+
     return (
         <div className='inner collection-inner'>
             <div className="t-wrap">
-                <h2 className='tit'>TOCOBO COLLECTION</h2>
+                <h2 className='tit'>TOCOBO<br /> COLLECTION</h2>
                 <p className="txt">Shop By Category
                 </p>
             </div>
             <Swiper
+                className='collection-slider'
                 slidesPerView={3}
                 spaceBetween={30}
                 loop={true}
                 pagination={{
                     type: 'progressbar',
                 }}
-                navigation={true}
+                onSwiper={(swiper) => (swiperRef.current = swiper)}
+                // navigation={true}
                 modules={[Pagination, Navigation]}
             >
                 {collectionData.map((sl, i) => (
@@ -30,12 +52,12 @@ const Collection = () => {
 
                         <a href="#">
                             <div className="info-wrap">
-                                <div className="info-tit"></div>
-                                <div className="name"></div>
-                                <div className="k-name"></div>
+                                <div className="info-tit">{sl.title}</div>
+                                <div className="name">{sl.name}</div>
+                                <div className="k-name">{sl.kname}</div>
                             </div>
                             <div className="img-wrap">
-                                <img src={sl.image} alt="" />
+                                <img src={sl.image} alt={sl.name} />
                             </div>
                         </a>
                     </SwiperSlide>
@@ -44,8 +66,8 @@ const Collection = () => {
             {/* </SwiperSlide> */}
 
 
-            <a href="#" className="prev">prev</a>
-            <a href="#" className="next">next</a>
+            <a href="#" className="prev" ref={prevRef}></a>
+            <a href="#" className="next" ref={nextRef}></a>
         </div>
     )
 }
